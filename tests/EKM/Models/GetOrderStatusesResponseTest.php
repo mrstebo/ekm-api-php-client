82e3887909716952cc71caeb62c0671ddbdc645e
Steven Atkinson <steven.atkinson@gmx.com>
2019-05-12 22:59:11 +0100

    Added the GetOrderStatusesResponse model.

---
+++ tests/EKM/Models/GetOrderStatusesResponseTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use EKM\Models\GetOrderStatusesResponse;
+use EKM\Models\OrderStatus;
+
+final class GetOrderStatusesResponseTest extends TestCase
+{
+    public function testGetStatuses()
+    {
+        $response = new GetOrderStatusesResponse([
+            'data' => [
+                'statuses' => array([
+                    'name' => 'Pending',
+                    'colour' => '#000099',
+                    'show_in_quick_stats' => true
+                ])
+            ]
+        ]);
+        $statuses = $response->getStatuses();
+
+        $this->assertEquals(1, sizeof($statuses));
+        $this->assertInstanceOf(OrderStatus::class, $statuses[0]);
+        $this->assertEquals('Pending', $statuses[0]->getName());
+        $this->assertEquals('#000099', $statuses[0]->getColour());
+        $this->assertTrue($statuses[0]->getShowInQuickStats());
+    }
+}
