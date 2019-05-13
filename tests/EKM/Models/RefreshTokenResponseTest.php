71c57dba564a7c25755549864bfc7ffc14ab9977
Steven Atkinson <steven.atkinson@gmx.com>
2019-05-13 07:41:18 +0100

    Created a RefreshTokenResponse model.

---
+++ tests/EKM/Models/RefreshTokenResponseTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use EKM\Models\RefreshTokenResponse;
+
+final class RefreshTokenResponseTest extends TestCase
+{
+    public function testConstructorUsingNull()
+    {
+        $response = new RefreshTokenResponse(null);
+
+        $this->assertEquals('', $response->getAccessToken());
+        $this->assertEquals('', $response->getRefreshToken());
+        $this->assertEquals(0, $response->getExpiresIn());
+    }
+
+    public function testGetAccessToken()
+    {
+        $response = new RefreshTokenResponse([ 'access_token' => 'test' ]);
+
+        $this->assertEquals('test', $response->getAccessToken());
+    }
+
+    public function testGetRefreshToken()
+    {
+        $response = new RefreshTokenResponse([ 'refresh_token' => 'test' ]);
+
+        $this->assertEquals('test', $response->getRefreshToken());
+    }
+
+    public function testGetExpiresIn()
+    {
+        $response = new RefreshTokenResponse([ 'expires_in' => 3600 ]);
+
+        $this->assertEquals(3600, $response->getExpiresIn());
+    }
+}
